eb54e7a8ee69a7adab21be536ec31d7273d40b97
Paulo Galvão <paulo.galvao@estsetubal.ips.pt>
2023-03-25 22:38:53 +0000

    Add files via upload

--- wx_random2_table.py
+++ wx_random2_table.py
@@ -7,26 +7,26 @@ class MainFrame(wx.Frame):
         super(MainFrame, self).__init__(parent, title=title, size=(600, 400))
         
         self.temperatura = []
-        self.umidade = []
+        self.humidade = []
         self.tabela_dados = []
         
         self.panel = wx.Panel(self)
         
         # Criando os elementos de interface gráfica
         self.label_temp = wx.StaticText(self.panel, label="Temperatura:")
-        self.label_umid = wx.StaticText(self.panel, label="Umidade:")
+        self.label_humid = wx.StaticText(self.panel, label="Humidade:")
         self.text_temp = wx.TextCtrl(self.panel, style=wx.TE_READONLY)
-        self.text_umid = wx.TextCtrl(self.panel, style=wx.TE_READONLY)
+        self.text_humid = wx.TextCtrl(self.panel, style=wx.TE_READONLY)
         self.gauge_temp = wx.Gauge(self.panel, range=50, size=(300, 20), style=wx.GA_HORIZONTAL|wx.GA_SMOOTH)
-        self.gauge_umid = wx.Gauge(self.panel, range=80, size=(300, 20), style=wx.GA_HORIZONTAL|wx.GA_SMOOTH)
+        self.gauge_humid = wx.Gauge(self.panel, range=80, size=(300, 20), style=wx.GA_HORIZONTAL|wx.GA_SMOOTH)
         font = wx.Font(10, wx.DEFAULT, wx.NORMAL, wx.NORMAL)
         self.gauge_temp.SetFont(font)
-        self.gauge_umid.SetFont(font)
+        self.gauge_humid.SetFont(font)
         self.list_ctrl = wx.ListCtrl(self.panel, style=wx.LC_REPORT)
         self.list_ctrl.InsertColumn(0, "Data")
         self.list_ctrl.InsertColumn(1, "Hora")
         self.list_ctrl.InsertColumn(2, "Temperatura")
-        self.list_ctrl.InsertColumn(3, "Umidade")
+        self.list_ctrl.InsertColumn(3, "Humidade")
         
         # Posicionando os elementos na janela
         vbox = wx.BoxSizer(wx.VERTICAL)
@@ -38,9 +38,9 @@ class MainFrame(wx.Frame):
         hbox1.Add(self.label_temp, flag=wx.RIGHT, border=8)
         hbox1.Add(self.text_temp, proportion=1)
         hbox2.Add(self.gauge_temp, proportion=1, flag=wx.EXPAND)
-        hbox3.Add(self.label_umid, flag=wx.RIGHT, border=8)
-        hbox3.Add(self.text_umid, proportion=1)
-        hbox4.Add(self.gauge_umid, proportion=1, flag=wx.EXPAND)
+        hbox3.Add(self.label_humid, flag=wx.RIGHT, border=8)
+        hbox3.Add(self.text_humid, proportion=1)
+        hbox4.Add(self.gauge_humid, proportion=1, flag=wx.EXPAND)
         hbox5.Add(self.list_ctrl, proportion=1, flag=wx.EXPAND)
         vbox.Add(hbox1, flag=wx.EXPAND|wx.LEFT|wx.RIGHT|wx.TOP, border=10)
         vbox.Add(hbox2, flag=wx.EXPAND|wx.LEFT|wx.RIGHT|wx.TOP, border=10)
@@ -59,34 +59,34 @@ class MainFrame(wx.Frame):
     def atualizar_dados(self, event):
         # Gerando números aleatórios para temperatura e umidade
         nova_temp = round(random.uniform(0, 50), 2)
-        nova_umid = round(random.uniform(20, 80), 2)
+        nova_humid = round(random.uniform(20, 80), 2)
         
         # Atualizando as listas de temperatura e umidade
         self.temperatura.append(nova_temp)
-        self.umidade.append(nova_umid)
+        self.humidade.append(nova_humid)
         if len(self.temperatura) > 60:
             self.temperatura.pop(0)
-        if len(self.umidade) > 60:
-            self.umidade.pop(0)
+        if len(self.humidade) > 60:
+            self.humidade.pop(0)
         
         # Atualizando os valores de temperatura e umidade nos controles de texto
         self.text_temp.SetValue(str(nova_temp))
-        self.text_umid.SetValue(str(nova_umid))
+        self.text_humid.SetValue(str(nova_humid))
         
         # Atualizando as barras de progresso de temperatura e umidade
         self.gauge_temp.SetValue(int(nova_temp))
-        self.gauge_umid.SetValue(int(nova_umid))
+        self.gauge_humid.SetValue(int(nova_humid))
 
         # Atualizando a tabela com os últimos 10 números lidos
-        self.tabela_dados.append((time.strftime("%d/%m/%Y"), time.strftime(" %H:%M:%S"), nova_temp, nova_umid))
+        self.tabela_dados.append((time.strftime("%d/%m/%Y"), time.strftime(" %H:%M:%S"), nova_temp, nova_humid))
         if len(self.tabela_dados) > 10:
             self.tabela_dados.pop(0)
         self.list_ctrl.DeleteAllItems()
-        for i, (data, hora, temp, umid) in enumerate(self.tabela_dados):
+        for i, (data, hora, temp, humid) in enumerate(self.tabela_dados):
             self.list_ctrl.InsertItem(i, data)
             self.list_ctrl.SetItem(i, 1, hora)
             self.list_ctrl.SetItem(i, 2, str(temp))
-            self.list_ctrl.SetItem(i, 3, str(umid))
+            self.list_ctrl.SetItem(i, 3, str(humid))
 
 
 if __name__ == '__main__':
